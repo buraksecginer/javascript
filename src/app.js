b1356a88696e035b7b471009a09d1a31f4fede29
Burak Seçginer <brkscgnr@gmail.com>
2021-10-19 00:41:20 +0300

    test

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-console.log("Selamunaleykum");
-
 let credits = ['A','B','C','D'];
 
 console.log('<ul>');
+
 for (let index = 0; index < credits.length; index++) {
     const element = credits[index]; 
     console.log('<li>'+element+'</li>');
 }
+
 console.log('</ul>');
